0767f008511d41587afabc7bed589bb20f6dc922
RumbleJack56 <ujjwalkakar2005@gmail.com>
2024-06-27 00:49:42 +0530

    Everything Working, Code Restructured, ISL Output Acquired

--- utils/ISLGenerator/utils.py
+++
@@ -1,8 +0,0 @@
-from nltk.parse.stanford import StanfordParser
-
-# Initialize the Stanford Parser
-sp = StanfordParser(
-    path_to_jar='path/to/stanford-parser-full-2018-02-27/stanford-parser.jar',
-    path_to_models_jar='path/to/stanford-parser-full-2018-02-27/stanford-parser-3.9.1-models.jar'
-)
-
