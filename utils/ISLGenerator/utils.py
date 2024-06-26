8636f79fbb9f3e8e5a7f87c1b80c4cc9a58ddc85
tarun jain <99501770+tarun4632@users.noreply.github.com>
2024-06-26 12:00:25 +0530

    Update utils.py

--- utils/ISLGenerator/utils.py
+++ utils/ISLGenerator/utils.py
@@ -2,7 +2,7 @@ from nltk.parse.stanford import StanfordParser
 
 # Initialize the Stanford Parser
 sp = StanfordParser(
-    path_to_jar='stanford-parser-full-2018-02-27/stanford-parser.jar',
-    path_to_models_jar='stanford-parser-full-2018-02-27/stanford-parser-3.9.1-models.jar'
+    path_to_jar='path/to/stanford-parser-full-2018-02-27/stanford-parser.jar',
+    path_to_models_jar='path/to/stanford-parser-full-2018-02-27/stanford-parser-3.9.1-models.jar'
 )
 
